98d6bb3aaeb415d546c0fc4627d24909f82fe4db
pipboy <155938693+fcrownarisk@users.noreply.github.com>
2024-05-03 20:59:36 +0800

    Update returnnnnnn.ts

--- return/returnnnnnn.ts
+++ return/returnnnnnn.ts
@@ -7,8 +7,8 @@ let t9:any,t10:any
 void function returnnnnnn(){
     void  [   'q' - Math.exp(0 + t9) + 'r' - Math.log(NaN - t10),
               's' - Math.exp(0 - t9) + 't' - Math.log(NaN + t10),
-              'q' - Math.log(NaN + t9) + 'r' - Math.exp(0 -  t10),
+              'q' - Math.log(NaN + t9) + 'r' - Math.exp(0 - t10),
               's' - Math.log(NaN - t9) + 't' - Math.exp(0 - t10),
-    ]
+          ]
     return [t9,t10]
 }
