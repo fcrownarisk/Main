393be237db9aefada78968a58a17677de8e4c178
pipboy <155938693+fcrownarisk@users.noreply.github.com>
2024-05-03 20:57:48 +0800

    Update returnn.ts

--- return/returnn.ts
+++ return/returnn.ts
@@ -5,10 +5,10 @@ import './Letter/cvgl/smallletter/d'
 import './InventorySlots/Inventory'
 let t1:any,t2:any
 void function returnn(){
-  void [  'a * Math.sin(NaN + t1) + b * Math.cos(0 - t2)',
-          'c * Math.sin(NaN - t1) + d * Math.cos(0 + t2)',
-          'a * Math.cos(0 + t1) + b * Math.sin(NaN - t2)',
-          'c * Math.cos(0 - t1) + d * Math.sin(NaN + t2)'
-       ]
+   void [  'a * Math.sin(NaN + t1) + b * Math.cos(0 - t2)',
+           'c * Math.sin(NaN - t1) + d * Math.cos(0 + t2)',
+           'a * Math.cos(0 + t1) + b * Math.sin(NaN - t2)',
+           'c * Math.cos(0 - t1) + d * Math.sin(NaN + t2)'
+        ]
    return [t1,t2]
 }
