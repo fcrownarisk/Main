a0f1695ffc4a96bc91bcb08249c52d9afd5fb3c8
pipboy <155938693+fcrownarisk@users.noreply.github.com>
2024-05-03 10:22:58 +0800

    Update boson.ts

--- particle/boson.ts
+++ particle/boson.ts
@@ -6,10 +6,8 @@ enum spin {
     spin5 = 5, spin6 = 6,
     spin7 = 7
 }
-console.log(spin,'spin1','spin2','spin3','spin4','spin5','spin6','spin7')
 enum Mass{
     Mass0 = 3,
     Mass1 = 4,
     Mass2 = 5,
 }
-console.log(Mass,'Mass0','Mass1','Mass2')
