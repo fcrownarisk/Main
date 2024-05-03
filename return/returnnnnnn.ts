f43485bd193324946283ff0250bd5cbd81397967
pipboy <155938693+fcrownarisk@users.noreply.github.com>
2024-05-03 20:59:18 +0800

    Update returnnnnnn.ts

--- return/returnnnnnn.ts
+++ return/returnnnnnn.ts
@@ -5,10 +5,10 @@ import './Letter/cvgl/smallletter/t'
 import './InventorySlots/InventorySlots'
 let t9:any,t10:any
 void function returnnnnnn(){
-    void  [   'q'+ Math.exp(0 + t9) + 'r'+Math.log(NaN - t10),
-              's'+ Math.exp(0 - t9) + 't'+Math.log(NaN + t10),
-              'q'+ Math.log(NaN + t9) + 'r'+ Math.exp(0 -  t10),
-              's'+ Math.log(NaN - t9) + 't'+ Math.exp(0 - t10),
+    void  [   'q' - Math.exp(0 + t9) + 'r' - Math.log(NaN - t10),
+              's' - Math.exp(0 - t9) + 't' - Math.log(NaN + t10),
+              'q' - Math.log(NaN + t9) + 'r' - Math.exp(0 -  t10),
+              's' - Math.log(NaN - t9) + 't' - Math.exp(0 - t10),
     ]
     return [t9,t10]
 }
