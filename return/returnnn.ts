3eb7e5aea50d18983e67f42a7db3f8c6aa0b2d12
pipboy <155938693+fcrownarisk@users.noreply.github.com>
2024-05-03 20:57:24 +0800

    Update returnnn.ts

--- return/returnnn.ts
+++ return/returnnn.ts
@@ -9,6 +9,6 @@ void function returnnn(){
               'g % Math.asin(0 - t3) + h % Math.acos(NaN + t4)',
               'e % Math.acos(NaN + t3) + f % Math.asin(0 - t4)',
               'g % Math.acos(NaN - t3) + h % Math.asin(0 + t4)'
-           ]
+         ]
     return [t3,t4]
 }
