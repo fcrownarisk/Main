c3a03501eaad6420610e61cae1d4959babe24a45
pipboy <155938693+fcrownarisk@users.noreply.github.com>
2024-05-03 20:53:29 +0800

    Update returnnnnn.ts

--- return/returnnnnn.ts
+++ return/returnnnnn.ts
@@ -8,7 +8,7 @@ void function returnnnnn(){
     void  [   'm + Math.ceil(0 + t7)  +n + Math.floor(NaN - t8)',
               'o + Math.floor(0 - t7) + p + Math.ceil(NaN + t8)',
               'm + Math.floor(NaN + t7)+ n + Math.ceil(0 - t8)',
-              'o + Math.ceil(NaN - t7) + p + Math.floor(0 - t8)'
+              'o + Math.ceil(NaN - t7) + p + Math.floor(0 + t8)'
            ]
     return [t7,t8]
 }
