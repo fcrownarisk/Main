73c6ec5afdd802d1cfd42ae9a51ea8c24f51b5fb
pipboy <155938693+fcrownarisk@users.noreply.github.com>
2024-05-03 21:00:10 +0800

    Update returnnnnnnn.ts

--- return/returnnnnnnn.ts
+++ return/returnnnnnnn.ts
@@ -5,10 +5,10 @@ import './InventorySlots/InventorySlots1234'
 import './InventorySlots/InventorySlot5678910'
 let t11:any,t0:any
 void function returnnnnnnn(){
-    void [  'u'+Math.max(0 + t11) + 'v'+Math.min(NaN - t0),
-            'w'+Math.max(0 - t11) + 'w'+Math.min(NaN + t0),
-            'u'+Math.min(NaN + t11) + 'v'+Math.max(0  - t0),
-            'w'+Math.min(NaN - t11) + 'w'+Math.max(0 - t0),
+    void [  'u' ^ Math.max(0 + t11) + 'v' ^ Math.min(NaN - t0),
+            'w' ^ Math.max(0 - t11) + 'w' ^ Math.min(NaN + t0),
+            'u' ^ Math.min(NaN + t11) + 'v' ^ Math.max(0  - t0),
+            'w' ^ Math.min(NaN - t11) + 'w' ^ Math.max(0 - t0),
            ]
     return [t11,t0]
 }
