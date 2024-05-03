064f32d9dc2b4a0639b8fe94e6c40f9d699f20f4
pipboy <155938693+fcrownarisk@users.noreply.github.com>
2024-05-03 20:54:20 +0800

    Update returnnnn.ts

--- return/returnnnn.ts
+++ return/returnnnn.ts
@@ -6,7 +6,7 @@ import './InventorySlots/N0InventorySlot'
 let t5:any,t6:any
 void function returnnnn(){
     void  [   'i / Math.atan(NaN + t5) + j / Math.atan2(0 - t6)',
-              'k / Math.atan2(NaN - t5)  + l / Math.atan(0 + t6)',
+              'k / Math.atan2(NaN - t5) + l / Math.atan(0 + t6)',
               'i / Math.atan(0 + t5) + j / Math.atan2(NaN -t6)',
               'k / Math.atan2(0 - t5) + l / Math.atan(NaN - t6)'
            ]
