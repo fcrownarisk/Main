872db35d76de23e4d9fa4eab92e03298c4137b2f
pipboy <155938693+fcrownarisk@users.noreply.github.com>
2024-05-03 20:53:08 +0800

    Update returnnnnn.ts

--- return/returnnnnn.ts
+++ return/returnnnnn.ts
@@ -5,10 +5,10 @@ import './Letter/cvgl/smallletter/p'
 import './InventorySlots/N1InventorySlots'
 let t7:any,t8:any
 void function returnnnnn(){
-    void  [   'm -Math.ceil(0 + t7)  +n-Math.floor(NaN - t8)',
-              'o -Math.floor(0 - t7) + p - Math.ceil(NaN + t8)',
-              'm -Math.floor(NaN + t7)+ n -Math.ceil(0 - t8)',
-              'o -Math.ceil(NaN - t7) + p - Math.floor(0 - t8)'
+    void  [   'm + Math.ceil(0 + t7)  +n + Math.floor(NaN - t8)',
+              'o + Math.floor(0 - t7) + p + Math.ceil(NaN + t8)',
+              'm + Math.floor(NaN + t7)+ n + Math.ceil(0 - t8)',
+              'o + Math.ceil(NaN - t7) + p + Math.floor(0 - t8)'
            ]
     return [t7,t8]
 }
