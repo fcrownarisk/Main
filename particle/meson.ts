5a718ccaf406fff1c5a9bd7fb5c5e9170a09340e
pipboy <155938693+fcrownarisk@users.noreply.github.com>
2024-05-03 10:23:48 +0800

    Update meson.ts

--- particle/meson.ts
+++ particle/meson.ts
@@ -5,19 +5,19 @@ import './XYZT'
 enum PAI{
     ABCD, EFGH, IJKL, MNOP,QRST ,UVW ,XYZ
 }
-console.log(JSON.stringify(PAI),'ABCD','EFGH','IJKL','MNOP','QRST','UVW','XYZ')
+console.log(JSON.stringify(PAI))
 enum MULTIPLE{
     abc,def,ghi,jkl,mno,pqr,stu,vwx,yz
 }
-console.log(JSON.stringify(MULTIPLE),'abc', 'def', 'ghi', 'jkl', 'mno','pqr', 'stu', 'vwx', 'yz')
+console.log(JSON.stringify(MULTIPLE))
 enum det{
     ABCD,"abc*EFGH","def*HIJK","ghi*LMNO","jkl*PQRT",'stu*TUVW','vwx*XYZ'
 }
-console.log(JSON.parse('det'),'ABCD','abc*EFGH','def*HIJK',"ghi*LMNO","jkl*PQRT",'ijk*PQRS','stu*TUVW','vwx*XYZ')
+console.log(JSON.parse('det'))
 enum trace{
      x1y1z1t1,x2y2z2t2,x3y3z3t3,t4z4y4x4,t5z5y5x5,t6z6y6x6
 }
-console.log(JSON.parse('trace'),'x1y1z1t1','x2y2z2t2','x3y3z3t3','t4z4y4x4','t5z5y5x5','t6z6y6x6')
+console.log(JSON.parse('trace'))
 enum dettrace{
      det,trace}
 console.log(dettrace)
@@ -28,8 +28,6 @@ console.log(tracedet)
 enum PAIMULTILE{
      PAI,MULTIPLE
 }
-console.log('PAI','MULTIPLE',PAIMULTILE)
 enum MULTIPLEPAI{
      MULTIPLE,PAI
 }
-console.log('MULTIPLE','PAI',MULTIPLEPAI)
