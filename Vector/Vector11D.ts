5248f842f8b56d7074bdfba86e77213650d583cf
Ark <155938693+fcrownarisk@users.noreply.github.com>
2024-06-13 09:51:08 +0800

    Update Vector11D.ts

--- Vector/Vector11D.ts
+++ Vector/Vector11D.ts
@@ -7,39 +7,7 @@ import '../Vector/Vector7D'
 import '../Vector/Vector8D'
 import '../Vector/Vector9D'
 import '../Vector/Vector10D'
- void function Vector2D(){
-    void Vector2D
-    return [Vector2D]
-}
-void function Vector3D(){
-    void Vector3D
-    return [Vector3D]
-}
-void function Vector4D(){
-    void Vector4D
-    return [Vector4D]
-}
-void function Vector5D(){
-    void Vector5D
-    return [Vector5D]
-}
-void function Vector6D(){
-    void Vector6D
-    return [Vector6D]
-}
-void function Vector7D(){
-     void Vector7D
-    return [Vector7D]
-}
-void function Vector8D(){
-    void Vector8D
-    return [Vector8D]
-}
-void function Vector9D(){
-    void Vector9D
-    return [Vector9D]
-}
-void function Vector10D(){
-    void Vector10D
-    return [Vector10D]
+export Vector(){
+    this.Vector2D + this.Vector3D + this.Vector4D = this.Vector5D + this.Vector6D + this.Vector7D
+    this.Vector8D + this.Vevtor9D = this.Vector10D + this.Vector11D
 }
