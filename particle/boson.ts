7e72447f78d0b7dfc0689c2fbe66a89e6d112567
fcrownarisk <155938693+fcrownarisk@users.noreply.github.com>
2024-06-24 15:40:47 +0800

    Update boson.ts

--- particle/boson.ts
+++ particle/boson.ts
@@ -1,6 +1,6 @@
 import './state/Cohereststate'
 import './state/Degeneratestate'
-enum spin {
+enum spin{
     spin1 = 1, spin2 = 2,
     spin3 = 3, spin4 = 4,
     spin5 = 5, spin6 = 6,
@@ -10,4 +10,9 @@ enum Mass{
     Mass0 = 3,
     Mass1 = 4,
     Mass2 = 5,
+}
+enum AngularMomentum{
+    [spin1 + spin2] * Mass0
+    [spin3 + spin4 + spin5] * Mass1
+    [spin6 + spin7] * Mass2
 }
