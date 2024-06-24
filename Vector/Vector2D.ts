8c9119a12498bade4c6bf30f496b3ff300300efc
fcrownarisk <155938693+fcrownarisk@users.noreply.github.com>
2024-06-24 15:38:01 +0800

    Update Vector2D.ts

--- Vector/Vector2D.ts
+++ Vector/Vector2D.ts
@@ -2,6 +2,6 @@ import './Vector2D'
 import '../Bigletter/I'
 import '../Bigletter/J'
 void function Vector2D(){
-   void Vector2D;['I*x' + 'J*y']
+   void Vector2D ['I*x' + 'J*y']
     return [Vector2D]
 }
