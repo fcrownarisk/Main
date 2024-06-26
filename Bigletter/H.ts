67984134432430bcbc88e2343b61a4b2f82b5e54
fcrownarisk <155938693+fcrownarisk@users.noreply.github.com>
2024-06-26 11:37:41 +0800

    Update H.ts

--- Bigletter/H.ts
+++ Bigletter/H.ts
@@ -1,15 +1,15 @@
  void function H(){
 return
  [
-   "type||typeof",
-   "interface&&namespace",
-   "extends//implements",
+   "type || typeof",
+   "interface && namespace",
+   "extends // implements",
  
-   "as\\infer",
-   "ture!=false",
-   "public||&private",
+   "as \\ infer",
+   "ture != false",
+   "public ||& private",
 
-   "this&||super",
-   "const&||&let",
-   "import===export"
+   "this &|| super",
+   "const &||& let",
+   "import == export"
 ]}
