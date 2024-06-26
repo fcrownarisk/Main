c03bb65ea5ed9fe58e0089d4b220a69d49041944
fcrownarisk <155938693+fcrownarisk@users.noreply.github.com>
2024-06-26 11:46:12 +0800

    Update I4.ts

--- fenymanndiagram/I4.ts
+++ fenymanndiagram/I4.ts
@@ -1,8 +1,6 @@
-
 import './fenymanndiagram/diagram'
 import './scatter.css'
 export function momentum(){
-    const I4=
-    'p1'+'p2'=='p3'+'p4'
+    const I4 = 'p1'+'p2'=='p3'+'p4'
     return 'scatter.css'
 }
