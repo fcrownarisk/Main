f6a0fe9faf966688627a032c7cace2555a9888cd
fcrownarisk <155938693+fcrownarisk@users.noreply.github.com>
2024-06-26 11:39:02 +0800

    Update and rename ABCD.ts to abcd.ts

---
+++ Letter/ABCDEFGH/abcd.ts
@@ -0,0 +1,10 @@
+ declare module a{}
+ declare module b{}
+ declare module c{}
+ declare module d{}
+ void function String(){
+`a:/=|,a:String;
+ b:|=/,b:string;
+ c:|=,c:string;
+ d:=|,d:string` 
+ }
