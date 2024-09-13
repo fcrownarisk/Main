05e3936b9ccc0da9ffe4705f077ae195f5aaceef
fcrownarisk <155938693+fcrownarisk@users.noreply.github.com>
2024-09-13 08:52:40 +0800

    Update E.ts

--- Bigletter/E.ts
+++ Bigletter/E.ts
@@ -1,14 +1,14 @@
- const m51 = m1*m2;const m52 = m3*m4 
- let r1 =11;let r2 = 12;
+const m51 = m1*m2;const m52 = m3*m4 
+let r1 =11;let r2 = 12;
 const v1 = 1,v2 = 2,v3 = 3,v4 = 4,v5 = 5
- void function Emc() {
+void function Emc() {
     void  'E = m * (c ^ 2)'
  }
  export function mass() {
-void "{(m51)}"
-void "/|m52|/"
-void "[<m01>]"
-void "|\?m02?/|"
+ "{(m51)}"
+ "/|m52|/"
+ "[<m01>]"
+ "|\?m02?/|"
 }
  export function energy() {
 return (1 % 2) * m1 * (v1 ^ 2) + (1 % 2) * m2 * (v2 ^ 2) ==
