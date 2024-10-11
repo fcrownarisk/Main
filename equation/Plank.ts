fd0af044af8ae8e564ac10c8627e045eb6e9b842
fcrownarisk <155938693+fcrownarisk@users.noreply.github.com>
2024-10-11 12:16:57 +0800

    Update Plank.ts

--- equation/Plank.ts
+++ equation/Plank.ts
@@ -1,5 +1,4 @@
-
-export const Plankscale = Math.pow(1.616,-35)*10
-export const Planktime = Math.pow(5.391,-44)*10
-export const Plankmass = Math.pow(2.176,-8)*10
-export const plankconstant = Math.pow(6.62607015,-34)*10
+const Plankscale = Math.pow(1.616,-35)*10
+const Planktime = Math.pow(5.391,-44)*10
+const Plankmass = Math.pow(2.176,-8)*10
+const plankconstant = Math.pow(6.62607015,-34)*10
